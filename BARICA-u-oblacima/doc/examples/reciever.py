2080ff8cd521d18e6fdd9cecd14195c234b9dd6b
Markus Schatten <markus.schatten@foi.hr>
2019-04-13 22:38:33 +0200

    Added a small hint to reciever.py that the connection is established.

--- BARICA-u-oblacima/doc/examples/reciever.py
+++ BARICA-u-oblacima/doc/examples/reciever.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
 
 
     port = connect( args.host, args.port )
+    print( "OK, B.A.R.I.C.A. is listening on port %d!" % port )
 
     # Start a random number generator program
     # (can be anything)
